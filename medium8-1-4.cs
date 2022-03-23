1810fa1586ff1cbc862f6eb85af46746c076e38a
Zubasty <kekshcgleb@gmail.com>
2022-03-23 11:09:40 +0300

    Create medium8-1-4.cs

---
+++ medium8-1-4.cs
@@ -0,0 +1,35 @@
+class Player
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }  
+    public Weapon Weapon { get; private set; }
+    public Movement Movement { get; private set; }
+
+    public void Move()
+    {
+        //Do move
+    }
+
+    public void Attack()
+    {
+        //attack
+    }
+
+    public bool IsReloading()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Weapon
+{
+    public float Cooldown { get; private set; }
+    public int Damage { get; private set; }
+}
+
+class Movement
+{
+    public float DirectionX { get; private set; }
+    public float DirectionY { get; private set; }
+    public float Speed { get; private set; }
+}
